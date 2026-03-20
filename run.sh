6bca245a049856080d9b77e430eeac034e1af0a0
Jerky676 <me@jerky676.com>
2026-03-20 08:27:48 -0500

    Fixed run file

--- run.sh
+++ run.sh
@@ -9,11 +9,8 @@ if [ ! -d "venv" ]; then
     python3 -m venv venv
 fi
 
-# Activate virtual environment
-source venv/bin/activate
-
 # Install requirements
-pip install -r requirements.txt
+venv/bin/pip install -r requirements.txt
 
 echo "▶️ Starting service..."
-python main.py
+venv/bin/python main.py
